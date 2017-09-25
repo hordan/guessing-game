4351e06d20562fc9d01b397c24136f03f8b6d5ec
hordan <daniilhorbach@gmail.com>
2017-09-25 10:53:56 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.ar = [];
+    this.mid = 0;
+    }
 
     setRange(min, max) {
+        this.max=max;
+        for(var i=0; i <=this.max;i++ ){
+        this.ar.push(i);
+        }
 
     }
 
     guess() {
+        this.mid = Math.floor(this.ar.length/2);
+        console.log(this.ar[this.mid]);
+        return this.ar[this.mid];
 
     }
 
     lower() {
-
+    this.ar.splice(this.mid+1, Number.MAX_VALUE)
     }
 
     greater() {
+        this.ar.splice(0,this.mid);
 
     }
 }
